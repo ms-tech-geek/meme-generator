633e0885f266675e1c3e8f3ce0adc1bffb4b83be
ms-tech-geek <thecorporator@gmail.com>
2024-09-09 18:40:54 +0530

    fetch memes data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+import { useEffect, useState } from "react";
+
 function App() {
-  return (
-    <>
-      Meme Generator
-      </>
-  );
+  const [memes,setMemes] = useState([])
+
+
+	useEffect(() => {
+    const fetchMemeData = async() => {
+        const response = await fetch(`https://api.imgflip.com/get_memes`);
+        const respData = await response.json();
+        setMemes(respData.data.memes);
+    }
+    fetchMemeData();
+  }, []);
+  {memes.length !=0 && console.log(memes[0]);}
+	return (
+    <div>
+      <h1>Meme Generator</h1>
+      <ul>
+        {memes.map(meme => (
+          <li key={meme.id}>
+            <p>{meme.name}</p>
+            <img src={meme.url} width='100px'/>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
 }
 
 export default App;
