b12ec324919df9222e5d2ecb35077437cdf77f0a
ms-tech-geek <thecorporator@gmail.com>
2024-09-11 19:44:22 +0530

    meme preview at run time

--- src/components/memes/SelectedMemeTemplate.js
+++ src/components/memes/SelectedMemeTemplate.js
@@ -17,18 +17,41 @@ const SelectedMemeTemplate = ({ meme, isOpen, onClose }) => {
 				bottomText: bottomRef.current.value,
 			})
 		);
-		dispatch(memeActions.setMemeGenerationProgress('meme-preview'))
+		dispatch(memeActions.setMemeGenerationProgress("meme-preview"));
+	};
+
+	const addMemeTextHandler = (event) => {
+		dispatch(
+			memeActions.addCaptionsToSelectedTemplate({
+				topText: topRef.current.value,
+				bottomText: bottomRef.current.value,
+			})
+		);
 	};
 
 	return (
 		<Modal isOpen={isOpen} onClose={onClose}>
 			<div className="selected-meme-template">
 				<h3>Selected Meme Template</h3>
+				<div className="meme-preview">
+					<h2 className="meme-text top-text">{topRef.current.value}</h2>
+					<img src={url} alt={name} />
+					<h2 className="meme-text bottom-text">{bottomRef.current.value}</h2>
+				</div>
 				<div>
 					<form onSubmit={onSubmitHandler}>
-						<input type="text" ref={topRef} placeholder="Add Top Text" />
-						<img src={url} alt={name} />
-						<input type="text" ref={bottomRef} placeholder="Add Bottom Text" />
+						<input
+							type="text"
+							ref={topRef}
+							onChange={addMemeTextHandler}
+							placeholder="Add Top Text"
+						/>
+						<input
+							type="text"
+							ref={bottomRef}
+							onChange={addMemeTextHandler}
+							placeholder="Add Bottom Text"
+						/>
 						<button>Generate Meme</button>
 					</form>
 				</div>
